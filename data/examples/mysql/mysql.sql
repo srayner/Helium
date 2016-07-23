ca8a013f0e982fa0ee9435c721cb68f66f0c8276
Steve Rayner <srayner02@gmail.com>
2016-07-23 18:45:02 +0100

    Updated db schema.

--- data/examples/mysql/mysql.sql
+++ data/examples/mysql/mysql.sql
@@ -8,15 +8,16 @@ CREATE TABLE gallery (
 
 -- Photograph table
 CREATE TABLE photograph (
-  `id`          Integer(11) NOT NULL AUTO_INCREMENT,
-  `title`       VarChar(128),
-  `caption`     Text,
-  `filename`    VarChar(256),
+  `id`                Integer(11) NOT NULL AUTO_INCREMENT,
+  `gallery_id`        Integer(11) NOT NULL,
+  `title`             VarChar(128),
+  `caption`           Text,
+  `filename`          VarChar(256),
+  `original_filename` VarChar(256),
+  `height`            Integer(11),
+  `width`             Integer(11),
+  `date_taken`        Date,
+  `location`          VarChar(256),
+  FOREIGN KEY (gallery_id) REFERENCES gallery(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE TABLE gallery_photograph (
-  `gallery_id` Integer(11) NOT NULL,
-  `photograph_id` Integer(11) NOT NULL,
-  PRIMARY KEY (gallery_id, photograph_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
