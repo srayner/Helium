c3481c682161075fdb9aab6e393646636c54a4c2
Steve Rayner <srayner02@gmail.com>
2016-07-24 09:36:36 +0100

    Added doctrine configuration.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -3,6 +3,23 @@
 namespace Application;
 
 return [
+    
+    // Doctrine
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ),
+            ),
+        ),
+    ),
+    
     'router' => [
         'routes' => [
             'home' => [
