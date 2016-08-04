7c0f58e2799d144d3334937d2271e17fd767ff52
Steve Rayner <srayner02@gmail.com>
2016-08-05 00:00:02 +0100

    Updated controller.

--- module/Application/src/Application/Controller/ImageController.php
+++ module/Application/src/Application/Controller/ImageController.php
@@ -20,22 +20,16 @@ class ImageController extends AbstractActionController
         $form = $this->service->getForm();
         
         $request = $this->getRequest();
-        if($request->isPost()) { 
-            $files =  $request->getFiles()->toArray();
-          //  die(var_dump($files));
-            $httpadapter = new \Zend\File\Transfer\Adapter\Http(); 
-            $filesize  = new \Zend\Validator\File\Size(array('min' => 1000 )); //1KB  
-            $extension = new \Zend\Validator\File\Extension(array('extension' => array('jpg')));
-            $httpadapter->setValidators(array($extension, $filesize), $files['file']['name']);
-            if($httpadapter->isValid()) {
-                
-                $httpadapter->setDestination('data/private/');
-                if($httpadapter->receive($files['file']['name'])) {
-                    die('ok');
-                    $newfile = $httpadapter->getFileName(); 
-                    die(var_dump($newfile));
-                }
-                die(var_dump($httpadapter->getMessages()));
+        if ($request->isPost()) { 
+            $post = array_merge_recursive(
+                    $request->getPost()->toArray(),
+                    $request->getFiles()->toArray()
+            );
+            $form->setData($post);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $this->setFileNames($data);
+                return $this->redirect()->toRoute('application/default');
             }
         } 
 
